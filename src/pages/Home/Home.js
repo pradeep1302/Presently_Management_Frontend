5bb57681cf59dd159e0a5f01b0ce450f5c566830
rishabhdasnitrr <ancientprin@gmail.com>
2024-11-13 19:04:49 +0530

    fixed landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import heroImg from "../../assets/hero.png";
-import navImg from "../../assets/medical-report.png";
+import navImg from "../../assets/icon.png";
 import "./Home.scss";
 import { Link } from "react-router-dom";
 import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
@@ -17,10 +17,10 @@ export default function Home() {
   return (
     <div className="wrapper">
       <section className="home">
-        <nav className="container --flex-between ">
+        <nav className="container --flex-between">
           <div className="logo">
             <img src={navImg} alt="navImg" className="navImg" />
-            <span className="nav-text">MEDIBOOK</span>
+            <span className="nav-text">Presently</span>
           </div>
 
           <ul className="home-links --flex-between">
@@ -47,66 +47,7 @@ export default function Home() {
             </ShowOnLogin>
           </ul>
         </nav>
-        <div className="container hero">
-          <SearchBar />
-        </div>
-        <div className="container hero">
-          <GoChevronDown
-            style={{
-              paddingBottom: "10px",
-              fontSize: "70px",
-              color: "white",
-            }}
-          />
-        </div>
       </section>
-      <section className="sec2">
-        <div className="container features cont">
-          <div className="divFeat">
-            Avoid Long Queues By Online Appoinment booking
-          </div>
-          <img
-            src={queue}
-            alt="queue image"
-            style={{
-              maxWidth: "500px",
-              width: "50%",
-              boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
-            }}
-          />
-        </div>
-        <div className="container features">
-          <img
-            src={paperWork}
-            alt="queue image"
-            style={{
-              maxWidth: "500px",
-              width: "50%",
-              boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
-            }}
-          />
-          <div className="divFeat">Manage Prescriptions Easily</div>
-        </div>
-        <div className="container features cont">
-          <div className="divFeat">
-            Helpful For Doctors in Managing Patients
-          </div>
-          <img
-            src={doctor}
-            alt="queue image"
-            style={{
-              maxWidth: "500px",
-              width: "50%",
-              boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
-            }}
-          />
-        </div>
-      </section>
-      <div class="bottom-container">
-        <p id="copyright" style={{ margin: "0" }}>
-          © Pradeep Agrawal.
-        </p>
-      </div>
     </div>
   );
 }
